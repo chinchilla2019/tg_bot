78eb264d677e4d123534123107f840ae5c570634
Kate <57578064+chinchilla2019@users.noreply.github.com>
2023-04-15 12:29:44 +1000

    Add files via upload

---
+++ db.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+
+# работа с базой данных
+class Database:
+    def __init__(self, db_file):
+        self.connection = sqlite3.connect(db_file)
+        self.db = db_file
+        self.cursor = self.connection.cursor()
+
+    def user_exists(self, client_id):
+        with self.connection:
+            res = self.cursor.execute("""SELECT * FROM Users WHERE client_id=?""", (client_id,)).fetchall()
+            return bool(len(res))
+
+    def add_user(self, client_id):
+        with self.connection:
+            return self.cursor.execute("INSERT INTO Users ('client_id') VALUES (?)", (client_id,))
+
+    def set_user_name(self, client_id, user_name):
+        with self.connection:
+            return self.cursor.execute("""UPDATE Users SET user_name=? WHERE client_id=?""",
+                                       (user_name, client_id))
+
+    def get_user_name(self, client_id):
+        with self.connection:
+            res = self.cursor.execute("""SELECT * FROM Users WHERE client_id=?""", (client_id,)).fetchall()[0]
+            return res[2]
+
+    def set_signup(self, client_id, signup):
+        with self.connection:
+            return self.cursor.execute("""UPDATE Users SET signup=? WHERE client_id=?""",
+                                       (signup, client_id))
+
+    def get_signup(self, client_id):
+        with self.connection:
+            res = self.cursor.execute("""SELECT * FROM Users WHERE client_id=?""", (client_id,)).fetchall()[0]
+            for row in res:
+                signup = str(row)
+            return signup
---
+++ markups.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+# кнопки
+bth_description = KeyboardButton('Описание')
+btn_what_is_gpt = KeyboardButton('Что такое GPT3?')
+btn_how_to_ask = KeyboardButton('Как задать вопрос?')
+main_Menu = ReplyKeyboardMarkup(resize_keyboard=True)
+main_Menu.add(bth_description, btn_what_is_gpt, btn_how_to_ask)
